f540adc02c97da454e165fbfa02e4826e69bfc61
rmonico <rmonico1@gmail.com>
2011-11-19 16:58:59 -0200

    Adicionado TODO

--- viewer/br/zero/controlefinanceiro/commandlineparser/LancamentoListSwitches.java
+++ viewer/br/zero/controlefinanceiro/commandlineparser/LancamentoListSwitches.java
@@ -3,14 +3,19 @@ package br.zero.controlefinanceiro.commandlineparser;
 import br.zero.commandlineparser.CommandLineSwitch;
 
 public class LancamentoListSwitches {
-	
+
 	private String where;
 
-	@CommandLineSwitch(index=1)
+	/**
+	 * TODO Depois trocar por filtros mais adequados
+	 * 
+	 * @param value
+	 */
+	@CommandLineSwitch(index = 1)
 	public void setWhere(String value) {
 		this.where = value;
 	}
-	
+
 	public String getWhere() {
 		return where;
 	}
