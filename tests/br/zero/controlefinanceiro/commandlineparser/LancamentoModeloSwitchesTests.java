02cde6e5865ebabba230115120b115397c374397
rmonico <rmonico1@gmail.com>
2011-11-19 20:00:07 -0200

    Novo teste do parsing de inclusão de lançamento modelo

--- tests/br/zero/controlefinanceiro/commandlineparser/LancamentoModeloSwitchesTests.java
+++ tests/br/zero/controlefinanceiro/commandlineparser/LancamentoModeloSwitchesTests.java
@@ -4,12 +4,14 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import java.text.SimpleDateFormat;
+
 import org.junit.Test;
 
 import br.zero.switchesparser.ParserException;
 
 public class LancamentoModeloSwitchesTests extends CustomParserTests {
-	
+
 	private LancamentoModeloSwitches lancamentoModeloSwitches;
 
 	private void doLancamentoModeloLoad(String[] args, LancamentoModeloCommand lancamentoModeloCommand) throws ParserException {
@@ -31,18 +33,40 @@ public class LancamentoModeloSwitchesTests extends CustomParserTests {
 
 		return lancamentoModeloSwitches.getListSwitches();
 	}
-	
+
 	@Test
 	public void doListTest() throws ParserException {
 		LancamentoModeloListSwitches listSwitches = doLancamentoModeloListLoad(new String[] { "lancmodelo", "ls" });
 
 		assertNull("Nome do modelo", listSwitches.getModelo());
 	}
-	
+
 	@Test
 	public void doListTest2() throws ParserException {
-		LancamentoModeloListSwitches listSwitches = doLancamentoModeloListLoad(new String[] { "lancmodelo", "ls", "modelo"});
+		LancamentoModeloListSwitches listSwitches = doLancamentoModeloListLoad(new String[] { "lancmodelo", "ls", "modelo" });
 
 		assertEquals("Nome do modelo", "modelo", listSwitches.getModelo());
+	}
+
+	private LancamentoModeloAddSwitches doLancamentoModeloAddLoad(String[] args) throws ParserException {
+		doLancamentoModeloLoad(args, LancamentoModeloCommand.ADD);
+
+		assertNull("Switches de lista", lancamentoModeloSwitches.getListSwitches());
+		assertNotNull("Switches de add", lancamentoModeloSwitches.getAddSwitches());
+		assertNull("Switches de remove", lancamentoModeloSwitches.getRemoveSwitches());
+
+		return lancamentoModeloSwitches.getAddSwitches();
+	}
+
+	@Test
+	public void doAddTest() throws ParserException {
+		LancamentoModeloAddSwitches addSwitches = doLancamentoModeloAddLoad(new String[] { "lancmodelo", "add", "modelo", "19/nov/2011", "conta origem", "conta destino", "1.99" });
+
+		assertEquals("Nome", "modelo", addSwitches.getModelo());
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyyy");
+		assertEquals("Data", "19/Nov/2011", sdf.format(addSwitches.getData().getTime()));
+		assertEquals("Conta origem", "conta origem", addSwitches.getContaOrigem());
+		assertEquals("Conta destino", "conta destino", addSwitches.getContaDestino());
+		assertEquals("Valor", 1.99, addSwitches.getValor(), 0);
 	}
 }
