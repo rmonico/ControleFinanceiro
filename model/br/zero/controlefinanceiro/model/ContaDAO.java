485ddc65a8a62ade16f32ebc048ca09aa9acd76d
rmonico <rmonico1@gmail.com>
2011-11-23 13:19:10 -0200

    dddd

--- model/br/zero/controlefinanceiro/model/ContaDAO.java
+++ model/br/zero/controlefinanceiro/model/ContaDAO.java
@@ -1,7 +1,5 @@
 package br.zero.controlefinanceiro.model;
 
-import java.util.List;
-
 import javax.persistence.Query;
 
 import br.zero.customdao.CustomDAO;
@@ -14,14 +12,7 @@ public class ContaDAO extends CustomDAO<Conta> {
 		Query q = getEntityManager().createQuery("select c from Conta c where c.nome = :nome");
 		q.setParameter("nome", nomeConta);
 
-		@SuppressWarnings("unchecked")
-		List<Conta> results = q.getResultList();
-
-		if (results.size() > 0) {
-			return results.get(0);
-		} else {
-			return null;
-		}
+		return (Conta) q.getSingleResult();
 	}
 
 }
