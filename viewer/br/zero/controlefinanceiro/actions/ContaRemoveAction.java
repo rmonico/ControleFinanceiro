377c026f9dca7847e531e722b512eea6193fccbd
rmonico <rmonico1@gmail.com>
2011-11-26 11:30:54 -0200

    Codificação de remoção de conta

--- viewer/br/zero/controlefinanceiro/actions/ContaRemoveAction.java
+++ viewer/br/zero/controlefinanceiro/actions/ContaRemoveAction.java
@@ -1,13 +1,62 @@
 package br.zero.controlefinanceiro.actions;
 
+import br.zero.controlefinanceiro.ControleFinanceiroException;
+import br.zero.controlefinanceiro.commandlineparser.ContaRemoveSwitches;
+import br.zero.controlefinanceiro.model.Conta;
+import br.zero.controlefinanceiro.model.ContaDAO;
 import br.zero.tinycontroller.Action;
 
 public class ContaRemoveAction implements Action {
 
-	@Override
-	public void run(Object param) {
-		// TODO Auto-generated method stub
+	private class ContaRemoveException extends ControleFinanceiroException {
 
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -1702349740543718569L;
+
+		public ContaRemoveException(String message) {
+			super("conta rm: " + message);
+		}
+		
+	}
+	
+	private ContaRemoveSwitches switches;
+
+	@Override
+	public void run(Object param) throws ContaRemoveException {
+		
+		switches = checkParamValid(param);
+		
+		ContaDAO contaDAO = new ContaDAO();
+		
+		Conta conta = contaDAO.getByNome(switches.getContaNome());
+		
+		if (conta == null) {
+			throw new ContaRemoveException("Conta de origem não encontrada (\"" + switches.getContaNome() + "\").");
+		}
+		
+		contaDAO.excluir(conta);
+		
+		System.out.println();
+		
+		System.out.println("-- Conta removida --");
+		
+		System.out.println(conta);
+	}
+
+	private ContaRemoveSwitches checkParamValid(Object param) throws ContaRemoveException {
+		if (!(param instanceof ContaRemoveSwitches)) {
+			throw new ContaRemoveException("Parametro deve ser um ContaRemoveSwitches.");
+		}
+		
+		ContaRemoveSwitches switches = (ContaRemoveSwitches) param;
+		
+		if (switches.getContaNome() ==  null) {
+			throw new ContaRemoveException("Nome da conta deve ser informado.");
+		}
+		
+		return switches;
 	}
 
 }
