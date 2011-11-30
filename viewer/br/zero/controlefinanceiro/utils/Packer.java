3b5c2f68d628eb77774a9390294a38c0c3ab9216
rmonico <rmonico1@gmail.com>
2011-11-30 14:23:22 -0200

    LANCAMENTO_LANCAMENTOCONTABILIZAVEL_PACKAGER -> LANCAMENTO_LANCAMENTOCONTABILIZAVEL_PACKER

--- viewer/br/zero/controlefinanceiro/utils/Packer.java
+++ viewer/br/zero/controlefinanceiro/utils/Packer.java
@@ -5,7 +5,7 @@ import br.zero.controlefinanceiro.model.Lancamento;
 public interface Packer<Packed extends Contabilizavel, ToPack> {
 	public Packed pack(ToPack p);
 
-	public static final Packer<LancamentoContabilizavel, Lancamento> LANCAMENTO_LANCAMENTOCONTABILIZAVEL_PACKAGER = new Packer<LancamentoContabilizavel, Lancamento>() {
+	public static final Packer<LancamentoContabilizavel, Lancamento> LANCAMENTO_LANCAMENTOCONTABILIZAVEL_PACKER = new Packer<LancamentoContabilizavel, Lancamento>() {
 		@Override
 		public LancamentoContabilizavel pack(Lancamento lancamento) {
 			LancamentoContabilizavel lancamentoContabilizavel = new LancamentoContabilizavel();
