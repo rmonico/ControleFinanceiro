7427c32ac24361a0c38603c4926b32e9c7df63f3
rmonico <rmonico1@gmail.com>
2011-12-01 10:34:06 -0200

    Novo switch: lancamentoRange

--- src/br/zero/controlefinanceiro/commandlineparser/ModeloAnalyseSwitches.java
+++ src/br/zero/controlefinanceiro/commandlineparser/ModeloAnalyseSwitches.java
@@ -3,11 +3,13 @@ package br.zero.controlefinanceiro.commandlineparser;
 import java.util.Calendar;
 
 import br.zero.commandlineparser.CommandLineSwitch;
+import br.zero.types.DateRange;
 
 public class ModeloAnalyseSwitches {
 
 	private String nomeModelo;
 	private Calendar dataBase;
+	private DateRange lancamentoRange;
 
 	
 	public String getNomeModelo() {
@@ -26,6 +28,15 @@ public class ModeloAnalyseSwitches {
 	@CommandLineSwitch(index=2, parser="MonthDateParser.parseCalendar")
 	public void setDataBase(Calendar database) {
 		this.dataBase = database;
+	}
+	
+	@CommandLineSwitch(param={"-lr", "--lancamento-range"}, parser="UtilsParser.parseDateRange")
+	public void setLancamentoRange(DateRange value) {
+		lancamentoRange = value;
+	}
+	
+	public DateRange getLancamentoRange() {
+		return lancamentoRange;
 	}
 
 }
