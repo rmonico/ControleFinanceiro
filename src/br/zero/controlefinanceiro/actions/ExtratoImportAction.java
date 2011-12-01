08e1ffe4894cdaae93fc6e381539bc185e72b137
rmonico <rmonico1@gmail.com>
2011-12-01 21:37:31 -0200

    Criados switches

--- src/br/zero/controlefinanceiro/actions/ExtratoImportAction.java
+++ src/br/zero/controlefinanceiro/actions/ExtratoImportAction.java
@@ -1,12 +1,45 @@
 package br.zero.controlefinanceiro.actions;
 
+import br.zero.controlefinanceiro.commandlineparser.ExtratoImportSwitches;
 import br.zero.tinycontroller.Action;
 
 public class ExtratoImportAction implements Action {
 
-	@Override
-	public void run(Object param) throws Exception {
-		throw new RuntimeException("Not implemented yet...");
+	private ExtratoImportSwitches switches;
+
+	public class ExtratoImportException extends Exception {
+
+		public ExtratoImportException(String message) {
+			super(message);
+		}
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -3059453211748572183L;
+
 	}
 
+	@Override
+	public void run(Object param) throws Exception {
+		switches = checkParamValid(param);
+	}
+
+	private ExtratoImportSwitches checkParamValid(Object param) throws ExtratoImportException {
+		if (!(param instanceof ExtratoImportSwitches)) {
+			throw new ExtratoImportException("Parâmetro deve ser um ExtratoImportSwitches.");
+		}
+		
+		ExtratoImportSwitches switches = (ExtratoImportSwitches) param;
+		
+		if (switches.getNomeConta() ==  null) {
+			throw new ExtratoImportException("Nome da conta deve ser informada.");
+		}
+		
+		if (switches.getNomeArquivo() == null) {
+			throw new ExtratoImportException("Nome do arquivo deve ser informado.");
+		}
+		
+		return switches;
+	}
 }
--- src/br/zero/controlefinanceiro/commandlineparser/ExtratoImportSwitches.java
+++ src/br/zero/controlefinanceiro/commandlineparser/ExtratoImportSwitches.java
@@ -1,5 +1,27 @@
 package br.zero.controlefinanceiro.commandlineparser;
 
+import br.zero.commandlineparser.CommandLineSwitch;
+
 public class ExtratoImportSwitches {
+	private String nomeConta;
+	private String nomeArquivo;
+
+	public String getNomeConta() {
+		return nomeConta;
+	}
+
+	@CommandLineSwitch(index = 1)
+	public void setNomeConta(String nomeConta) {
+		this.nomeConta = nomeConta;
+	}
+
+	public String getNomeArquivo() {
+		return nomeArquivo;
+	}
+
+	@CommandLineSwitch(index = 2)
+	public void setNomeArquivo(String nomeArquivo) {
+		this.nomeArquivo = nomeArquivo;
+	}
 
 }
