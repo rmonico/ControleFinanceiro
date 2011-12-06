93c47496e48dd285901c902a8ab749617ee93325
rmonico <rmonico1@gmail.com>
2011-12-06 16:51:55 -0200

    Novo switch de nome do banco

--- src/br/zero/controlefinanceiro/commandlineparser/ExtratoAnalyseSwitches.java
+++ src/br/zero/controlefinanceiro/commandlineparser/ExtratoAnalyseSwitches.java
@@ -1,5 +1,18 @@
 package br.zero.controlefinanceiro.commandlineparser;
 
+import br.zero.commandlineparser.CommandLineSwitch;
+
 public class ExtratoAnalyseSwitches {
+
+	private String nomeBanco;
+
+	@CommandLineSwitch(index = 1)
+	public void setNomeBanco(String nomeBanco) {
+		this.nomeBanco = nomeBanco;
+	}
+
+	public String getNomeBanco() {
+		return nomeBanco;
+	}
 
 }
