b9336bea70a4d6cafacf526bc63ef855f02d5c1b
rmonico <rmonico1@gmail.com>
2011-12-06 13:56:31 -0200

    Nova entidade: ReferenciaExtrato

---
+++ model/br/zero/controlefinanceiro/model/extrato/ReferenciaExtrato.java
@@ -0,0 +1,68 @@
+package br.zero.controlefinanceiro.model.extrato;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import br.zero.controlefinanceiro.model.Conta;
+import br.zero.customdao.EntitySetup;
+
+@Entity
+@Table(name = "referenciaextrato", schema = "controlefinanceiro_extrato")
+@EntitySetup(findAllQuery = "select re from ReferenciaExtrato re a order by re.id, re.banco.id, re.conta.id, re.referencia", findByIdQuery = "select re from ReferenciaExtrato re where re.id=:id", idFieldName = "id")
+public class ReferenciaExtrato {
+
+	@Id
+	@SequenceGenerator(name = "CONTROLEFINANCEIRO_EXTRATO.REFERENCIAEXTRATO_ID_SEQ", sequenceName = "CONTROLEFINANCEIRO_EXTRATO.REFERENCIAEXTRATO_ID_SEQ", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "CONTROLEFINANCEIRO_EXTRATO.REFERENCIAEXTRATO_ID_SEQ")
+	private Integer id;
+
+	@ManyToOne
+	@JoinColumn(name = "BancoID")
+	private Conta banco;
+
+	@ManyToOne
+	@JoinColumn(name = "ContaID")
+	private Conta conta;
+
+	private String referencia;
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Conta getBanco() {
+		return banco;
+	}
+
+	public void setBanco(Conta banco) {
+		this.banco = banco;
+	}
+
+	public Conta getConta() {
+		return conta;
+	}
+
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
+
+	public String getReferencia() {
+		return referencia;
+	}
+
+	public void setReferencia(String referencia) {
+		this.referencia = referencia;
+	}
+
+}
