e10e1811da12ca8957aac14123a3cbab53e9db9e
rmonico <rmonico1@gmail.com>
2011-12-10 21:31:50 -0200

    Versão preliminar da ação

--- src/br/zero/controlefinanceiro/actions/ExtratoAnalyseAction.java
+++ src/br/zero/controlefinanceiro/actions/ExtratoAnalyseAction.java
@@ -1,12 +1,129 @@
 package br.zero.controlefinanceiro.actions;
 
+import java.util.List;
+
+import br.zero.controlefinanceiro.commandlineparser.ExtratoAnalyseSwitches;
+import br.zero.controlefinanceiro.model.Conta;
+import br.zero.controlefinanceiro.model.ContaDAO;
+import br.zero.controlefinanceiro.model.ExtratoLine;
+import br.zero.controlefinanceiro.model.ExtratoParser;
+import br.zero.controlefinanceiro.model.Lancamento;
+import br.zero.controlefinanceiro.model.LancamentoDAO;
+import br.zero.controlefinanceiro.model.extrato.ExtratoLancamento;
+import br.zero.controlefinanceiro.model.extrato.ExtratoLancamentoDAO;
+import br.zero.controlefinanceiro.utils.ControleFinanceiroException;
 import br.zero.tinycontroller.Action;
 
 public class ExtratoAnalyseAction implements Action {
 
+	private LancamentoDAO lancamentoDAO;
+
+	private class ExtratoAnalyseException extends ControleFinanceiroException {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -1702349740543718569L;
+
+		public ExtratoAnalyseException(String message) {
+			super("extrato analyse: " + message);
+		}
+
+	}
+
 	@Override
 	public void run(Object param) throws Exception {
+		ExtratoAnalyseSwitches switches = checkParamValid(param);
+
+		Conta banco = getConta(switches.getNomeBanco());
+
+		List<Lancamento> lancamentoSemExtratoList = getLancamentoSemExtratoList();
+
+		List<ExtratoLancamento> extratoLancamentoOrfao = getExtratoLancamentoOrfao();
+
+		ContaDAO contaDAO = new ContaDAO();
+
+		ExtratoParser parser = banco.getParser();
+
+		for (ExtratoLancamento linhaExtrato : extratoLancamentoOrfao) {
+			for (int i = 0; i < lancamentoSemExtratoList.size(); i++) {
+				Lancamento lancamentoSemExtrato = lancamentoSemExtratoList.get(i);
+
+				parser.parse(linhaExtrato.getOriginal());
+
+				ExtratoLine line = parser.getLine();
+
+				Conta contaExtrato = contaDAO.resolveExtratoLine(banco, line.getReferencia());
+
+				Conta contaOrigemEsperada;
+				Conta contaDestinoEsperada;
+
+				if (lancamentoSemExtrato.getValor() > 0) {
+					// Dinheiro saiu da conta
+					contaOrigemEsperada = banco;
+					contaDestinoEsperada = contaExtrato;
+				} else {
+					// Dinheiro entrou na conta
+					contaOrigemEsperada = contaExtrato;
+					contaDestinoEsperada = banco;
+				}
+
+				if (extratoLineMatch(lancamentoSemExtrato, line, contaOrigemEsperada, contaDestinoEsperada)) {
+					lancamentoSemExtrato.setExtrato(linhaExtrato);
+					
+					lancamentoDAO.alterar(lancamentoSemExtrato);
+				}
+			}
+		}
+
 		throw new RuntimeException("Not implemented yet...");
+	}
+
+	private boolean extratoLineMatch(Lancamento lancto, ExtratoLine line, Conta contaOrigemEsperada, Conta contaDestinoEsperada) {
+		boolean origemOk = lancto.getContaOrigem().equals(contaOrigemEsperada);
+		boolean destinoOk = lancto.getContaDestino().equals(contaDestinoEsperada);
+		boolean valorOk = lancto.getValor().equals(line.getValor());
+		
+		return origemOk && destinoOk && valorOk;
+	}
+
+	private Conta getConta(String nomeConta) throws ExtratoAnalyseException {
+		ContaDAO dao = new ContaDAO();
+
+		Conta conta = dao.getByNome(nomeConta);
+
+		if (conta == null) {
+			throw new ExtratoAnalyseException("Conta \"" + nomeConta + "\" não encontrada...");
+		}
+
+		return conta;
+	}
+
+	private List<ExtratoLancamento> getExtratoLancamentoOrfao() {
+		ExtratoLancamentoDAO dao = new ExtratoLancamentoDAO();
+		
+		return dao.listarOrfaos();
+	}
+
+	private List<Lancamento> getLancamentoSemExtratoList() {
+		lancamentoDAO = new LancamentoDAO();
+		
+		return lancamentoDAO.listarSemExtrato();
+	}
+
+	private ExtratoAnalyseSwitches checkParamValid(Object param) throws ExtratoAnalyseException {
+		if (!(param instanceof ExtratoAnalyseSwitches)) {
+			throw new ExtratoAnalyseException("Parametro deve ser um ExtratoAnalyseSwitches.");
+		}
+
+
+		ExtratoAnalyseSwitches switches = (ExtratoAnalyseSwitches) param;
+		
+		if (switches.getNomeBanco() == null) {
+			throw new ExtratoAnalyseException("");
+		}
+		
+		return switches;
 	}
 
 }
