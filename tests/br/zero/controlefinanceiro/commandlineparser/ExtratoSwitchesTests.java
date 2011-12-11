ae3c5a3aa13f7b5374903efb44cc8a5da603ba8c
rmonico <rmonico1@gmail.com>
2011-12-11 03:11:46 -0200

    Teste do parser de linha de comando do extrato

---
+++ tests/br/zero/controlefinanceiro/commandlineparser/ExtratoSwitchesTests.java
@@ -0,0 +1,29 @@
+package br.zero.controlefinanceiro.commandlineparser;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import br.zero.commandlineparser.ParserException;
+
+public class ExtratoSwitchesTests extends CustomParserTests {
+
+	private void doExtratoLoad(String[] args, Command command) throws ParserException {
+		doLoad(args);
+
+		assertEquals("Comando", command, switches.getEntity());
+	}
+
+	@Test
+	public void doListTest() throws ParserException {
+		doExtratoLoad(new String[] {"extrato-ls", "conta"}, Command.EXTRATO_LIST);
+		
+		assertNotNull(switches.getExtratoListSwitches());
+		
+		ExtratoListSwitches listSwitches = switches.getExtratoListSwitches(); 
+			
+		assertEquals("Nome da conta", "conta", listSwitches.getNomeConta());
+
+	}
+}
