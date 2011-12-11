bace1852325566c7f52abca49c3538ff93da3eed
rmonico <rmonico1@gmail.com>
2011-12-11 03:59:20 -0200

    Classe movida para pasta correta

--- tests/br/zero/controlefinanceiro/commandlineparser/ContaReferenciaSwitchesTests.java
+++ tests/br/zero/controlefinanceiro/commandlineparser/ContaReferenciaSwitchesTests.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import br.zero.commandlineparser.ParserException;
 
 public class ContaReferenciaSwitchesTests extends CustomParserTests {
-	
+
 	private void doContaReferenciaLoad(String[] args, Command command) throws ParserException {
 		doLoad(args);
 
@@ -24,8 +24,20 @@ public class ContaReferenciaSwitchesTests extends CustomParserTests {
 		ContaReferenciaListSwitches listSwitches = switches.getContaReferenciaListSwitches();
 
 		assertEquals("Nome da conta", "conta", listSwitches.getNomeConta());
-		assertEquals("Nome do banco", "conta", listSwitches.getNomeBanco());
+		assertEquals("Nome do banco", "banco", listSwitches.getNomeBanco());
 	}
 
+	@Test
+	public void doAddTest() throws ParserException {
+		doContaReferenciaLoad(new String[] { "ctaref-add", "conta", "banco", "regex" }, Command.CONTAREFERENCIA_ADD);
+
+		assertNotNull("switches", switches.getContaReferenciaAddSwitches());
+
+		ContaReferenciaAddSwitches addSwitches = switches.getContaReferenciaAddSwitches();
+
+		assertEquals("Nome da conta", "conta", addSwitches.getNomeConta());
+		assertEquals("Nome do banco", "banco", addSwitches.getNomeBanco());
+		assertEquals("Regex", "regex", addSwitches.getRegex());
+	}
 
 }
