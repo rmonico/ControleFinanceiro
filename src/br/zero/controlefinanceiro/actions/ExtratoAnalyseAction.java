f179302014240ccbde83287d0f20271ea8f6d4fb
rmonico <rmonico1@gmail.com>
2011-12-15 13:58:18 -0200

    Mudança na forma como as informações são apresentadas

--- src/br/zero/controlefinanceiro/actions/ExtratoAnalyseAction.java
+++ src/br/zero/controlefinanceiro/actions/ExtratoAnalyseAction.java
@@ -1,10 +1,12 @@
 package br.zero.controlefinanceiro.actions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import br.zero.controlefinanceiro.commandlineparser.ExtratoAnalyseSwitches;
 import br.zero.controlefinanceiro.model.Conta;
 import br.zero.controlefinanceiro.model.ContaDAO;
+import br.zero.controlefinanceiro.model.ExtratoBalanceLine;
 import br.zero.controlefinanceiro.model.ExtratoLine;
 import br.zero.controlefinanceiro.model.ExtratoLineParser;
 import br.zero.controlefinanceiro.model.ExtratoTransactionLine;
@@ -13,12 +15,60 @@ import br.zero.controlefinanceiro.model.LancamentoDAO;
 import br.zero.controlefinanceiro.model.extrato.ExtratoLancamento;
 import br.zero.controlefinanceiro.model.extrato.ExtratoLancamentoDAO;
 import br.zero.controlefinanceiro.utils.ControleFinanceiroException;
+import br.zero.controlefinanceiro.utils.ControleFinanceiroFormatters;
 import br.zero.controlefinanceiro.utils.ExtratoLineParserException;
+import br.zero.textgrid.TextGrid;
+import br.zero.textgrid.TextGridColumnAlignment;
+import br.zero.textgrid.TextGridException;
+import br.zero.textgrid.TextGridFormattedColumn;
 import br.zero.tinycontroller.Action;
 
 public class ExtratoAnalyseAction implements Action {
 
+	public class ExtratoLineAnalyseResult {
+
+		private Character tipo;
+		private String original;
+		private Conta conta;
+		private Lancamento lancamento;
+
+		public Character getTipo() {
+			return tipo;
+		}
+
+		public void setTipo(Character tipo) {
+			this.tipo = tipo;
+		}
+
+		public String getOriginal() {
+			return original;
+		}
+
+		public void setOriginal(String original) {
+			this.original = original;
+		}
+
+		public Conta getConta() {
+			return conta;
+		}
+
+		public void setConta(Conta conta) {
+			this.conta = conta;
+		}
+
+		public Lancamento getLancamento() {
+			return lancamento;
+		}
+
+		public void setLancamento(Lancamento lancamento) {
+			this.lancamento = lancamento;
+		}
+
+	}
+
 	private LancamentoDAO lancamentoDAO;
+	private Conta banco;
+	private ExtratoAnalyseSwitches switches;
 
 	private class ExtratoAnalyseException extends ControleFinanceiroException {
 
@@ -31,26 +81,28 @@ public class ExtratoAnalyseAction implements Action {
 			super("extrato analyse: " + message);
 		}
 
+		public ExtratoAnalyseException(Exception e) {
+			super(e);
+		}
+
 	}
 
 	@Override
-	public void run(Object param) throws Exception {
-		ExtratoAnalyseSwitches switches = checkParamValid(param);
+	public void run(Object param) throws ExtratoAnalyseException {
+		switches = checkParamValid(param);
 
-		Conta banco = getConta(switches.getNomeBanco());
+		banco = getConta(switches.getNomeBanco());
 
 		List<Lancamento> lancamentoSemExtratoList = getLancamentoSemExtratoList();
 
 		List<ExtratoLancamento> extratoLancamentoOrfao = getExtratoLancamentoOrfao(banco);
-		
+
 		if (extratoLancamentoOrfao.size() == 0) {
 			System.out.println();
 			System.out.println("Nenhum item de extrato órfão encontrado para o banco \"" + banco.getNome() + "\".");
-			
+
 			return;
 		}
-
-		ContaDAO contaDAO = new ContaDAO();
 
 		ExtratoLineParser parser = banco.getParser();
 
@@ -58,75 +110,123 @@ public class ExtratoAnalyseAction implements Action {
 			throw new ExtratoAnalyseException("Nenhum parser registrado para o banco: \"" + banco.getNome() + "\".");
 		}
 
-		makeSync(switches, banco, lancamentoSemExtratoList, extratoLancamentoOrfao, contaDAO, parser);
+		try {
+			makeSync(lancamentoSemExtratoList, extratoLancamentoOrfao, parser);
+		} catch (Exception e) {
+			throw new ExtratoAnalyseException(e);
+		}
 	}
 
-	private void makeSync(ExtratoAnalyseSwitches switches, Conta banco, List<Lancamento> lancamentoSemExtratoList, List<ExtratoLancamento> extratoLancamentoOrfao, ContaDAO contaDAO, ExtratoLineParser parser) throws ExtratoLineParserException {
+	private void makeSync(List<Lancamento> lancamentoSemExtratoList, List<ExtratoLancamento> extratoLancamentoOrfao, ExtratoLineParser parser) throws ExtratoLineParserException, TextGridException {
+		TextGrid grid = createGrid();
+
+		List<ExtratoLineAnalyseResult> statuses = new ArrayList<ExtratoLineAnalyseResult>();
+
+		ContaDAO contaDAO = new ContaDAO();
+
 		for (ExtratoLancamento linhaExtrato : extratoLancamentoOrfao) {
-			StringBuilder status = new StringBuilder();
 
 			parser.parse(linhaExtrato.getOriginal());
 
-			ExtratoLine l = parser.getLine();
-			
-			if (!(l instanceof ExtratoTransactionLine)) {
-				continue;
-			}
-			
-			ExtratoTransactionLine line = (ExtratoTransactionLine) l;
+			ExtratoLine line = parser.getLine();
 
-			status.append(line.getReferencia());
+			ExtratoLineAnalyseResult analyseResult;
 
-			Conta contaExtrato = contaDAO.resolveExtratoLine(banco, line.getReferencia());
-
-			if (contaExtrato == null) {
-				// TODO O catch all deve ser implementado aqui! Ver direito,
-				// pois no caso de linhas de saldo a referência não deve ser
-				// resolvida mesmo
-				status.append("  ==> Referência não resolvida!");
-
-				System.out.println(status);
-
-				continue;
+			if (line instanceof ExtratoBalanceLine) {
+				analyseResult = syncBalanceLine();
+			} else if (line instanceof ExtratoTransactionLine) {
+				analyseResult = syncTransactionLine(lancamentoSemExtratoList, contaDAO, linhaExtrato, (ExtratoTransactionLine) line);
+			} else {
+				analyseResult = showUnknownLine();
 			}
 
-			boolean extratoLineMatched = false;
-
-			for (Lancamento lancamentoSemExtrato : lancamentoSemExtratoList) {
-				Conta contaOrigemEsperada;
-				Conta contaDestinoEsperada;
-
-				if (lancamentoSemExtrato.getValor() > 0) {
-					// Dinheiro saiu da conta
-					contaOrigemEsperada = banco;
-					contaDestinoEsperada = contaExtrato;
-				} else {
-					// Dinheiro entrou na conta
-					contaOrigemEsperada = contaExtrato;
-					contaDestinoEsperada = banco;
-				}
-
-				if (extratoLineMatch(lancamentoSemExtrato, line, contaOrigemEsperada, contaDestinoEsperada)) {
-					extratoLineMatched = true;
-					lancamentoSemExtrato.setExtrato(linhaExtrato);
-
-					if (switches.getRealize()) {
-						lancamentoDAO.alterar(lancamentoSemExtrato);
-					}
-
-					status.append("  ==> " + lancamentoSemExtrato);
-
-					break;
-				}
-
-			}
-
-			if (!extratoLineMatched) {
-				status.append("  ==> Sem lançamento correspondente.");
-			}
-
-			System.out.println(status);
+			statuses.add(analyseResult);
 		}
+
+		grid.setValues(statuses);
+
+		grid.show();
+	}
+
+	private ExtratoLineAnalyseResult syncBalanceLine() {
+		ExtratoLineAnalyseResult result = new ExtratoLineAnalyseResult();
+		result.setTipo('B');
+
+		return result;
+	}
+
+	private ExtratoLineAnalyseResult syncTransactionLine(List<Lancamento> lancamentoSemExtratoList, ContaDAO contaDAO, ExtratoLancamento linhaExtrato, ExtratoTransactionLine line) {
+		ExtratoLineAnalyseResult result = new ExtratoLineAnalyseResult();
+		result.setTipo('T');
+
+		result.setOriginal(line.getOriginal());
+
+		Conta contaExtrato = contaDAO.resolveExtratoLine(banco, line.getReferencia());
+
+		if (contaExtrato == null) {
+			// TODO O catch all deve ser implementado aqui!
+			// TODO A resolução de referências na linha de comando deve ser
+			// implementada aqui!
+			return result;
+		}
+
+		for (Lancamento lancamentoSemExtrato : lancamentoSemExtratoList) {
+			if (lancamentoSemExtrato.getExtrato() != null) {
+				// Ignora estas linhas. Elas podem ocorrer pois os itens da
+				// lista lancamentoSemExtratoList são alterados neste loop
+				continue;
+			}
+
+			Conta contaOrigemEsperada;
+			Conta contaDestinoEsperada;
+
+			if (lancamentoSemExtrato.getValor() > 0) {
+				// Dinheiro saiu da conta
+				contaOrigemEsperada = banco;
+				contaDestinoEsperada = contaExtrato;
+			} else {
+				// Dinheiro entrou na conta
+				contaOrigemEsperada = contaExtrato;
+				contaDestinoEsperada = banco;
+			}
+
+			if (extratoLineMatch(lancamentoSemExtrato, line, contaOrigemEsperada, contaDestinoEsperada)) {
+				lancamentoSemExtrato.setExtrato(linhaExtrato);
+
+				if (switches.getRealize()) {
+					lancamentoDAO.alterar(lancamentoSemExtrato);
+				}
+
+				result.setLancamento(lancamentoSemExtrato);
+
+				break;
+			}
+
+		}
+
+		return result;
+	}
+
+	private ExtratoLineAnalyseResult showUnknownLine() {
+		ExtratoLineAnalyseResult result = new ExtratoLineAnalyseResult();
+
+		result.setTipo('?');
+
+		return result;
+	}
+
+	private TextGrid createGrid() {
+		TextGrid grid = new TextGrid();
+
+		grid.getData().setHeaderSeparatorChar('=');
+		grid.getData().setTitle("Sincronização de Extrato banco \"" + banco.getNome() + "\"");
+
+		TextGridFormattedColumn.createFormattedColumn(grid, "Tipo", TextGridFormattedColumn.STRING_FORMATTER, TextGridColumnAlignment.RIGHT, "getTipo");
+		TextGridFormattedColumn.createFormattedColumn(grid, "Original", TextGridFormattedColumn.STRING_FORMATTER, TextGridColumnAlignment.RIGHT, "getOriginal");
+		TextGridFormattedColumn.createFormattedColumn(grid, "Conta", ControleFinanceiroFormatters.CONTA_FORMATTER, TextGridColumnAlignment.RIGHT, "getConta");
+		TextGridFormattedColumn.createFormattedColumn(grid, "Lancamento", ControleFinanceiroFormatters.CONTA_FORMATTER, TextGridColumnAlignment.RIGHT, "getContaOrigem");
+
+		return grid;
 	}
 
 	private boolean extratoLineMatch(Lancamento lancto, ExtratoTransactionLine line, Conta contaOrigemEsperada, Conta contaDestinoEsperada) {
