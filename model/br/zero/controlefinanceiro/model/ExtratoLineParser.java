0f929583b15c6f78a80f215688a4d0b7ea38654a
rmonico <rmonico1@gmail.com>
2011-12-15 12:35:13 -0200

    Nova exceção sendo lançada por ExtratoLineParser.parse

--- model/br/zero/controlefinanceiro/model/ExtratoLineParser.java
+++ model/br/zero/controlefinanceiro/model/ExtratoLineParser.java
@@ -1,8 +1,10 @@
 package br.zero.controlefinanceiro.model;
 
+import br.zero.controlefinanceiro.utils.ExtratoParserException;
+
 public interface ExtratoLineParser {
 
-	void parse(String line);
+	void parse(String line) throws ExtratoParserException;
 
 	ExtratoLine getLine();
 	
