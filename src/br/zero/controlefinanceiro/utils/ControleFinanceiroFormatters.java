31d7f0308f25aec658f668b7a1dfe60124454c4c
rmonico <rmonico1@gmail.com>
2011-12-15 13:51:25 -0200

    LANCAMENTO_FORMATTER

--- src/br/zero/controlefinanceiro/utils/ControleFinanceiroFormatters.java
+++ src/br/zero/controlefinanceiro/utils/ControleFinanceiroFormatters.java
@@ -1,6 +1,9 @@
 package br.zero.controlefinanceiro.utils;
 
+import java.text.SimpleDateFormat;
+
 import br.zero.controlefinanceiro.model.Conta;
+import br.zero.controlefinanceiro.model.Lancamento;
 import br.zero.controlefinanceiro.model.modelo.Modelo;
 import br.zero.textgrid.TextGridException;
 import br.zero.textgrid.TextGridFormattedColumn;
@@ -8,54 +11,95 @@ import br.zero.textgrid.TextGridFormatter;
 
 public class ControleFinanceiroFormatters {
 
+	public static final TextGridFormatter LANCAMENTO_FORMATTER = createLancamentoFormatter();
 	public static final TextGridFormatter CONTA_FORMATTER = createContaFormatter();
 	public static final TextGridFormatter MODELO_FORMATTER = createModeloFormatter();
 
-	private static TextGridFormatter createContaFormatter() {
-		TextGridFormatter contaFormatter = new TextGridFormatter() {
-			
+	private static TextGridFormatter createLancamentoFormatter() {
+		TextGridFormatter lancamentoFormatter = new TextGridFormatter() {
+
 			@Override
 			public StringBuilder parse(Object cellValue) throws TextGridException {
 				if (cellValue == null) {
 					return TextGridFormattedColumn.NULL_FORMATTER.parse(cellValue);
 				}
+
+				if (!(cellValue instanceof Lancamento)) {
+					throw new TextGridException("LANCAMENTO_FORMATTER: Must be used only with br.zero.controlefinanceiro.model.Lancamento fields.");
+				}
+
+				Lancamento value = (Lancamento) cellValue;
+
+				StringBuilder finalValue = formatLancamento(value);
+
+				return finalValue;
+			}
+
+			private StringBuilder formatLancamento(Lancamento lancamento) {
+				SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyyy");
+
+				StringBuilder formatted = new StringBuilder();
 				
+				formatted.append((lancamento.getData() != null) ? (sdf.format(lancamento.getData().getTime())) : "[null]");
+				formatted.append(", ");
+				formatted.append((lancamento.getContaOrigem() != null) ? lancamento.getContaOrigem().getNome() : "[null]");
+				formatted.append(" -> ");
+				formatted.append((lancamento.getContaDestino() != null) ? lancamento.getContaDestino().getNome() : "[null]");
+				formatted.append(", ");
+				formatted.append((lancamento.getValor() != null) ? lancamento.getValor().toString() : "[null]");
+
+				return formatted;
+			}
+		};
+
+		return lancamentoFormatter;
+	}
+
+	private static TextGridFormatter createContaFormatter() {
+		TextGridFormatter contaFormatter = new TextGridFormatter() {
+
+			@Override
+			public StringBuilder parse(Object cellValue) throws TextGridException {
+				if (cellValue == null) {
+					return TextGridFormattedColumn.NULL_FORMATTER.parse(cellValue);
+				}
+
 				if (!(cellValue instanceof Conta)) {
 					throw new TextGridException("CONTA_FORMATTER: Must be used only with br.zero.controlefinanceiro.model.Conta fields.");
 				}
-				
+
 				Conta value = (Conta) cellValue;
-				
+
 				StringBuilder finalValue = new StringBuilder(value.getNome());
-				
+
 				return finalValue;
 			}
 		};
-		
+
 		return contaFormatter;
 	}
 
 	private static TextGridFormatter createModeloFormatter() {
 		TextGridFormatter modeloFormatter = new TextGridFormatter() {
-			
+
 			@Override
 			public StringBuilder parse(Object cellValue) throws TextGridException {
 				if (cellValue == null) {
 					return new StringBuilder("[null]");
 				}
-				
+
 				if (!(cellValue instanceof Modelo)) {
 					throw new TextGridException("MODELO_FORMATTER: Must be used only with br.zero.controlefinanceiro.model.Modelo fields.");
 				}
-				
+
 				Modelo value = (Modelo) cellValue;
-				
+
 				StringBuilder finalValue = new StringBuilder(value.getNome());
-				
+
 				return finalValue;
 			}
 		};
-		
+
 		return modeloFormatter;
 	}
 
