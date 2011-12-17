686a7e88c9816f4df7f0914b26dddd16ffc679be
rmonico <rmonico1@gmail.com>
2011-12-17 01:08:37 -0200

    método setOrigem

--- model/br/zero/controlefinanceiro/model/ParsedExtratoLancamento.java
+++ model/br/zero/controlefinanceiro/model/ParsedExtratoLancamento.java
@@ -3,5 +3,6 @@ package br.zero.controlefinanceiro.model;
 import br.zero.controlefinanceiro.model.extrato.ExtratoLancamento;
 
 public interface ParsedExtratoLancamento {
+	void setOrigem(ExtratoLancamento lancamento);
 	ExtratoLancamento getOrigem();
 }
