e5261b45ecf705d212bf8ee6d7ba9258ff66a21b
rmonico <rmonico1@gmail.com>
2011-12-16 22:08:21 -0200

    Tirados testes do parser do santander (estavam vazios). Criado teste para data do parser do itau

--- tests/br/zero/controlefinanceiro/commandlineparser/ExtratoParsersTests.java
+++ tests/br/zero/controlefinanceiro/commandlineparser/ExtratoParsersTests.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.text.SimpleDateFormat;
+
 import org.junit.Test;
 
 import br.zero.controlefinanceiro.model.ExtratoLine;
@@ -15,72 +17,38 @@ import br.zero.controlefinanceiro.utils.ExtratoParsers;
 public class ExtratoParsersTests {
 
 	private ExtratoLineParser parser;
-	
+
 	private ExtratoLineParser getItauParser() {
 		return ExtratoParsers.ITAU_EXTRATO_PARSER;
 	}
-	
+
 	@Test
 	public void doItauExtratoParserTransactionLineTest() throws ExtratoLineParserException {
 		parser = getItauParser();
-		
+
 		parser.parse("01/09			IOF		1,85-		352,31-	");
 
 		ExtratoLine el = parser.getLine();
-		
+
 		assertNotNull("linha do extrato não-nula", el);
-		
+
 		assertTrue("Implementa ExtratoTransactionLine", el instanceof ExtratoTransactionLine);
-		
+
 		ExtratoTransactionLine line = (ExtratoTransactionLine) el;
-		
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyyy");
+		assertEquals("data", "01/Sep/2011", sdf.format(line.getData().getTime()));
+
 		assertEquals("referência", "IOF", line.getReferencia());
 	}
 
 	@Test
 	public void doItauExtratoParserInvalidLineTests2() {
-//		parser = getItauParser();
-//		
-//		parser.parse("16/11\t\t\tSDO CTA/APL AUTOMATICAS\t\t\t\t1.258,52\t");
-//
-//		assertFalse("linha válida", parser.isTransferLine());
+		// parser = getItauParser();
+		//
+		// parser.parse("16/11\t\t\tSDO CTA/APL AUTOMATICAS\t\t\t\t1.258,52\t");
+		//
+		// assertFalse("linha válida", parser.isTransferLine());
 	}
 
-	@Test
-	public void doItauExtratoParserTests2() {
-//		parser = getItauParser();
-//
-//		parser.parse("16/11\t\t\tCXE 000323 SAQUE\t7619\t550,00\t-\t\t");
-//
-//		assertTrue("linha válida", parser.isTransferLine());
-//		TODO Depois será necessário ter o campo de data, o teste já está aí
-//		SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyyy");
-//		assertEquals("Data", "16/Nov/2011", sdf.format(parser.getData().getTime()));
-	}
-	
-	
-	private ExtratoLineParser getSantanderParser() {
-		return ExtratoParsers.SANTANDER_EXTRATO_PARSER;
-	}
-
-	@Test
-	public void doSantanderExtratoParserInvalidLineTests() {
-//		parser = getSantanderParser();
-//		
-//		parser.parse("16/11/2011 \t\tSALDO ANTERIOR\t0\t-1546,84\t-1546,84");
-//
-//		assertFalse("linha válida", parser.isTransferLine());
-	}
-
-	@Test
-	public void doSantanderExtratoParserTests() {
-//		parser = getSantanderParser();
-//
-//		parser.parse("16/11/2011 \t\tDEBITO AUTOM CONTA DE TELEFONE EMBRATEL TV SAT\t0\t-68,16\t0");
-//
-//		assertTrue("linha válida", parser.isTransferLine());
-//		TODO Depois será necessário ter o campo de data, o teste já está aí
-//		SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyyy");
-//		assertEquals("Data", "16/Nov/2011", sdf.format(parser.getData().getTime()));
-	}
 }
