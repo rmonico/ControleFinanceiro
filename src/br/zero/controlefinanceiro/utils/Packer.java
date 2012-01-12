c73416db8dffbbd2d8258c78065332174b4bda81
rmonico <rmonico1@gmail.com>
2012-01-12 06:36:18 -0200

    Atualização do planejamento

--- src/br/zero/controlefinanceiro/utils/Packer.java
+++ src/br/zero/controlefinanceiro/utils/Packer.java
@@ -5,6 +5,7 @@ import br.zero.controlefinanceiro.model.Lancamento;
 public interface Packer<Packed extends Contabilizavel, ToPack> {
 	public Packed pack(ToPack p);
 
+	// TODO Tirar esta implementação daqui e mover para o LancamentoListAction, só é usado la
 	public static final Packer<LancamentoContabilizavel, Lancamento> LANCAMENTO_LANCAMENTOCONTABILIZAVEL_PACKER = new Packer<LancamentoContabilizavel, Lancamento>() {
 		@Override
 		public LancamentoContabilizavel pack(Lancamento lancamento) {
