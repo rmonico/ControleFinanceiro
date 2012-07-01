5abcc8e30b5ba1fe8ac9420bce2233ab76ef5a52
rmonico <rmonico1@gmail.com>
2012-07-01 04:26:37 -0300

    Tirados erros de BackupAction (mas não está funcionando ainda)

--- src/br/zero/controlefinanceiro/actions/BackupAction.java
+++ src/br/zero/controlefinanceiro/actions/BackupAction.java
@@ -3,12 +3,11 @@ package br.zero.controlefinanceiro.actions;
 import br.zero.tinycontroller.Action;
 
 
-public class BackupAction implements Action {
+public class BackupAction implements Action<Object, Object> {
 
 	@Override
-	public void run(Object param) {
-		// TODO Auto-generated method stub
-		
+	public Object run(Object param) {
+		return null;
 	}
 
 }
