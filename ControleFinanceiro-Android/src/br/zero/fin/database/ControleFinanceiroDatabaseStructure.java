0d519ced05ad331fe5b13b07402c8fcfba0054d7
Rafael Monico <rmonico1@gmail.com>
2013-08-24 02:45:18 -0300

    Criação das tabelas adaptada para a nova estrutura

--- ControleFinanceiro-Android/src/br/zero/fin/database/ControleFinanceiroDatabaseStructure.java
+++ ControleFinanceiro-Android/src/br/zero/fin/database/ControleFinanceiroDatabaseStructure.java
@@ -3,10 +3,15 @@ package br.zero.fin.database;
 import java.util.ArrayList;
 import java.util.List;
 
-import br.zero.androidhelpers.database.RawDatabaseObjectStructure;
-import br.zero.androidhelpers.database.DatabaseStructure;
+import br.zero.androidhelpers.databasestructure.raw.DatabaseStructure;
+import br.zero.androidhelpers.databasestructure.raw.RawDatabaseObjectStructure;
+import br.zero.androidhelpers.databasestructure.table.TableToRawSQLAdapter;
+import br.zero.fin.model.Conta;
+import br.zero.fin.model.modelo.Modelo;
 
 public class ControleFinanceiroDatabaseStructure implements DatabaseStructure {
+
+	private int nextId;
 
 	public String getName() {
 		return "controlefinanceiro.sqlite";
@@ -16,92 +21,77 @@ public class ControleFinanceiroDatabaseStructure implements DatabaseStructure {
 		return 1;
 	}
 
-	public List<TableStructure> getTables() {
-		List<TableStructure> tables = new ArrayList<TableStructure>();
-
-		tables.add(createModeloModeloTableStructure());
-		tables.add(createContaTableStructure());
-
-		return tables;
-	}
-
-	private TableStructure createModeloModeloTableStructure() {
-		TableStructure table = new TableStructure() {
-
-			public String getDatabaseCreationSQL() {
-				return "CREATE TABLE modelo_modelo ("
-						+ "  id integer primary key autoincrement,"
-						+ "  nome text," + "  observacao text);";
-			}
-
-			public String getUpgradeSQL(int oldVersion, int newVersion) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			public void getPopulationSQLs(List<String> list) {
-				list.add("INSERT INTO \"modelo_modelo\" VALUES(1,'main',NULL);");
-			}
-
-		};
-
-		return table;
-	}
-
-	private TableStructure createContaTableStructure() {
-		TableStructure table = new TableStructure() {
-
-			public String getDatabaseCreationSQL() {
-				return "CREATE TABLE conta ("
-						+ "  _id integer primary key autoincrement,"
-						+ "  nome text,"
-						+ "  observacao text,"
-						+ "  contabilizavel boolean);";
-			}
-
-			public String getUpgradeSQL(int oldVersion, int newVersion) {
-				return "";
-			}
-
-			public void getPopulationSQLs(List<String> list) {
-				list.add("INSERT INTO \"conta\" VALUES(97,'academia',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(98,'aesa',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(99,'almoco',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(100,'cabelo',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(101,'carteira',NULL,1);");
-				list.add("INSERT INTO \"conta\" VALUES(102,'casa',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(103,'claro - 7653-0794',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(104,'claro - 96587-4452',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(105,'claro tv',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(106,'conferencia',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(107,'coop',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(108,'despesas bancarias',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(109,'diversos',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(110,'emprestimo',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(111,'greenline',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(112,'inss',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(113,'internet',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(114,'itau',NULL,1);");
-				list.add("INSERT INTO \"conta\" VALUES(115,'itau credito',NULL,1);");
-				list.add("INSERT INTO \"conta\" VALUES(116,'lanche',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(117,'luz',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(118,'medico',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(119,'metro',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(120,'salario',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(121,'santander credito',NULL,1);");
-				list.add("INSERT INTO \"conta\" VALUES(122,'semasa',NULL,0);");
-				list.add("INSERT INTO \"conta\" VALUES(123,'vivo',NULL,0);");
-			}
-
-		};
-
-		return table;
-	}
-
 	public void populateRawObjectStructures(
 			List<RawDatabaseObjectStructure> objects) {
-		// TODO Auto-generated method stub
-		
+		objects.add(new TableToRawSQLAdapter(new ModeloModeloTableStructure(), createModeloModeloInitialData()));
+		objects.add(new TableToRawSQLAdapter(new ContaTableStructure(), createContaInitialData()));
 	}
 
+	private List<Modelo> createModeloModeloInitialData() {
+		List<Modelo> data = new ArrayList<Modelo>();
+		
+		Modelo modelo = new Modelo();
+		modelo.setId(1);
+		modelo.setNome("Main");
+		
+		data.add(modelo);
+		
+		return data;
+	}
+
+	private List<Conta> createContaInitialData() {
+		List<Conta> data = new ArrayList<Conta>();
+		
+		nextId = 1;
+		
+		data.add(newConta("academia"));
+		data.add(newConta("aesa"));
+		data.add(newConta("almoco"));
+		data.add(newConta("cabelo"));
+		data.add(newContaContabilizavel("carteira"));
+		data.add(newConta("casa"));
+		data.add(newConta("claro - 7653-0794"));
+		data.add(newConta("claro - 96587-4452"));
+		data.add(newConta("claro tv"));
+		data.add(newConta("conferencia"));
+		data.add(newConta("despesas bancarias"));
+		data.add(newConta("diversos"));
+		data.add(newConta("emprestimo"));
+		data.add(newConta("greenline"));
+		data.add(newConta("inss"));
+		data.add(newConta("internet"));
+		data.add(newContaContabilizavel("itau"));
+		data.add(newConta("itau credito"));
+		data.add(newConta("lanche"));
+		data.add(newConta("luz"));
+		data.add(newConta("medico"));
+		data.add(newConta("metro"));
+		data.add(newContaContabilizavel("salario"));
+		data.add(newConta("santander credito"));
+		data.add(newConta("semasa"));
+		data.add(newConta("vivo"));
+		
+		return data;
+	}
+
+	
+	private Conta newContaContabilizavel(String nome) {
+		Conta conta = newConta(nome);
+		
+		// TODO Classe do modelo desatualizada
+//		conta.setContabilizavel(true);
+		
+		return conta;
+	}
+
+	private Conta newConta(String nome) {
+		Conta conta = new Conta();
+		
+		// TODO Testar o ++ nessa situação
+		conta.setId(nextId++);
+		conta.setNome(nome);
+		
+		return conta;
+	}
+	
 }
