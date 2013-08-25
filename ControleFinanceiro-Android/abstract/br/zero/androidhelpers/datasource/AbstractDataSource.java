a6988610885d3cdd2b91056028af43975cf6c6d6
Rafael Monico <rmonico@hacklab.com.br>
2013-08-25 16:53:05 -0300

    AbstractDataSource não é mais responsável por devolver a conexão com o banco de dados

--- ControleFinanceiro-Android/abstract/br/zero/androidhelpers/datasource/AbstractDataSource.java
+++ ControleFinanceiro-Android/abstract/br/zero/androidhelpers/datasource/AbstractDataSource.java
@@ -1,23 +1,6 @@
 package br.zero.androidhelpers.datasource;
 
-import br.zero.androidhelpers.databasestructure.raw.AbstractDBHelper;
-import br.zero.androidhelpers.databasestructure.raw.DatabaseStructure;
-import br.zero.fin.Application;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-
 public abstract class AbstractDataSource<T> implements DataSource<T> {
 
-	protected SQLiteDatabase getDatabase() {
-		AbstractDBHelper dbHelper = new AbstractDBHelper(Application.getAppContext(), DatabaseStructure.instance);
-		
-		SQLiteDatabase database = dbHelper.getWritableDatabase();
-		return database;
-	}
-
-	public abstract Cursor getCursorForAll();
-	
-	public abstract T convertCurrentPositionToModel(Cursor cursor);
-	
 	public abstract void create(T obj);
 }
