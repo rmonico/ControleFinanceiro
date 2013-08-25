8294648a0f635caa4d2de535dd5e934a2952ffb6
Rafael Monico <rmonico@hacklab.com.br>
2013-08-25 18:47:36 -0300

    Alteração na assinatura do método ProviderFactory.getCursorForAll

--- ControleFinanceiro-Android/src/br/zero/fin/datasource/ContaProviderFactory.java
+++ ControleFinanceiro-Android/src/br/zero/fin/datasource/ContaProviderFactory.java
@@ -2,16 +2,14 @@ package br.zero.fin.datasource;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import br.zero.android.helpers.appdefaults.AppDefaults;
 import br.zero.androidhelpers.datasource.ProviderFactory;
 import br.zero.fin.database.ContaTableStructure;
 import br.zero.fin.model.Conta;
 
 public class ContaProviderFactory implements ProviderFactory<Conta> {
 	
-	public ContaProviderFactory() {
-		
-	}
-
 	public Conta convertCurrentPositionToModel(Cursor cursor) {
 		Conta conta = new Conta();
 		
@@ -22,7 +20,12 @@ public class ContaProviderFactory implements ProviderFactory<Conta> {
 		return conta;
 	}
 
-	public Cursor getCursorForAll(SQLiteDatabase database) {
+	public Cursor getCursorForAll() {
+		SQLiteOpenHelper helper = AppDefaults.get.createHelper();
+		
+		SQLiteDatabase database = helper.getWritableDatabase();
+		
+		// TODO Esta Ž a œnica coisa que Ž espec’fica desta classe. Pode ser passada por invers‹o de controle
 		ContaTableStructure structure = ContaTableStructure.get;
 		
 		Cursor cursor = database.query(structure.getName(), structure.getFieldNames(), null, null, null, null, null);
