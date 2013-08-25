7a0452af87fcb3665fc7390df440785e4540e8a5
Rafael Monico <rmonico@hacklab.com.br>
2013-08-25 19:42:04 -0300

    Correção no tipo de campo inteiro

--- ControleFinanceiro-Android/abstract/br/zero/androidhelpers/databasestructure/table/FieldType.java
+++ ControleFinanceiro-Android/abstract/br/zero/androidhelpers/databasestructure/table/FieldType.java
@@ -2,7 +2,7 @@ package br.zero.androidhelpers.databasestructure.table;
 
 public interface FieldType {
 	
-	public static FieldType INTEGER = new ConcreteFieldType("int") {};
+	public static FieldType INTEGER = new ConcreteFieldType("integer") {};
 	public static FieldType TEXT = new ConcreteFieldType("text") {};
 	public static FieldType BOOLEAN = new ConcreteFieldType("boolean") {};
 	
