f86897f2d676d480dc6cfac34784d1fb547e0134
Rafael Monico <rmonico@hacklab.com.br>
2013-08-25 19:51:20 -0300

    Assinatura dos métodos mexida

--- ControleFinanceiro-Android/abstract/br/zero/androidhelpers/databasestructure/raw/RawDatabaseObjectStructure.java
+++ ControleFinanceiro-Android/abstract/br/zero/androidhelpers/databasestructure/raw/RawDatabaseObjectStructure.java
@@ -3,9 +3,8 @@ package br.zero.androidhelpers.databasestructure.raw;
 import java.util.List;
 
 public interface RawDatabaseObjectStructure {
-	
-	// TODO Fazer devolver uma lista de strings
-	void populateObjectCreationSQLs(List<String> sqls);
-	
-	void populateUpgradeSQL(List<String> sqls, int oldVersion, int newVersion);
+
+	List<String> populateObjectCreationSQLs();
+
+	List<String> populateUpgradeSQL(int oldVersion, int newVersion);
 }
