c759f08cedcbfc2c46cf6c667192ca2212cbaa40
Rafael Monico <rmonico@hacklab.com.br>
2013-08-25 21:17:11 -0300

    Lançando exceção quando não é possível chamar os getters ou setters

--- ControleFinanceiro-Android/abstract/br/zero/androidhelpers/databasestructure/raw/RawDatabaseObjectStructure.java
+++ ControleFinanceiro-Android/abstract/br/zero/androidhelpers/databasestructure/raw/RawDatabaseObjectStructure.java
@@ -2,9 +2,11 @@ package br.zero.androidhelpers.databasestructure.raw;
 
 import java.util.List;
 
+import br.zero.androidhelpers.databasestructure.table.DatabaseStructureException;
+
 public interface RawDatabaseObjectStructure {
 
-	List<String> getObjectCreationSQLs();
+	List<String> getObjectCreationSQLs() throws DatabaseStructureException;
 
-	List<String> getUpgradeSQL(int oldVersion, int newVersion);
+	List<String> getUpgradeSQL(int oldVersion, int newVersion) throws DatabaseStructureException;
 }
