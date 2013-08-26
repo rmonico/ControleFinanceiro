90ef2906802b8acf294414bd9049c8a80bcddecf
Rafael Monico <rmonico@hacklab.com.br>
2013-08-25 23:14:45 -0300

    Pequena correção

--- ControleFinanceiro-Android/abstract/br/zero/androidhelpers/databasestructure/table/FieldType.java
+++ ControleFinanceiro-Android/abstract/br/zero/androidhelpers/databasestructure/table/FieldType.java
@@ -1,12 +1,14 @@
 package br.zero.androidhelpers.databasestructure.table;
 
+import java.util.Calendar;
+
 public interface FieldType<T> {
 	
 	public static FieldType<Integer> INTEGER = new IntegerFieldType();
 	public static FieldType<String> TEXT = new StringFieldType();
 	public static FieldType<Boolean> BOOLEAN = new BooleanFieldType();
-	public static FieldType<?> DATE = new DateFieldType();
-	public static FieldType<?> REAL = new RealFieldType();
+	public static FieldType<Calendar> DATE = new DateFieldType();
+	public static FieldType<Double> REAL = new RealFieldType();
 	
 	Class<? extends T> getJavaCorrespondingType();
 
